130aac6da47d39427df347cbf4accf040d49bf7f
Fab-2727 <fabriziolionelsosa27@gmail.com>
2021-01-13 23:11:21 -0300

    Improvement in test

--- src/test/bash/simple-test.sh
+++ src/test/bash/simple-test.sh
@@ -56,10 +56,13 @@ UrlsOfMethods=(
 "http://localhost:8080/library/author/by-name?name="
 "http://localhost:8080/library/add-author"
 "http://localhost:8080/library/author/all"
+# New URLS:
+"http://localhost:8080/library/topic/by-id?id="
+"http://localhost:8080/library/stock/all"
 )
 COUNTER=1;
 
-for (( c=0; c<=20; c++ ))
+for (( c=0; c<=22; c++ ))
     do 
         echo -e "${YELLOW} ${c} - URL: ${UrlsOfMethods[c]} ${NC}"
         echo ""
@@ -70,11 +73,20 @@ echo "STARTING CURL's execution"
 # curl --header "Content-Type: application/json" 'http://localhost:8080/library/add-publisher' --request POST --data '{"username":"jonh doe"}'
 # CURL to get and send id as query param
 # curl --header "Content-Type: application/json" -X GET 'http://localhost:8080/library/publisher?id=1'
-echo "Testing only READ
+echo "Testing only READ methods"
 
+echo ""
+echo "All books"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[2]}"
+echo ""
 
 echo "All topics"
 curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[9]}"
+echo ""
+sleep 3
+
+echo "All stocks"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[21]}"
 echo ""
 sleep 3
 
@@ -86,47 +98,70 @@ echo "All authors"
 curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[19]}"
 echo ""
 sleep 3
+
 echo "##############################################################################"
 
-echo "Get Book by ID"
-
 echo ""
+echo "Get entities by QueryParam"
+echo ""
+
+echo "Get Book by ID"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[0]1}"
+echo ""
+sleep 3
 
 echo "Get Book by name"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[2]El Extranjero}"
 echo ""
+sleep 3
 
 echo "Get ALL books"
 echo ""
 
 echo "Get Books by category"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[3]narracion}"
 echo ""
+sleep 3
+
 echo "Get Books by authors name"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[4]albert camus}"
 echo ""
+sleep 3
 
 echo "Get books by author ID"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[5]1}"
 echo ""
+sleep 3
+
 echo "Get Stock by Book ID"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[7]1}"
 echo ""
-echo "Get author by ID"
+sleep 3
+
+echo "Get Topic by ID"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[20]1}"
 echo ""
-echo "Get Author by NAME"
-echo ""
-echo "Get ALL authors"
-echo ""
+sleep 3
 
 echo "Get topic by name"
 curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[10]}Drama"
 echo ""
 sleep 3
+
 echo "Get PUBLISHER by ID"
-curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[12]}1"
+curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[20]}1"
 echo ""
 sleep 3
+
 echo "Get Author by ID"
 curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[16]}1"
 echo ""
 sleep 3
+
 echo "Get Author by NAME"
 curl --header "Content-Type: application/json" -X GET "${UrlsOfMethods[17]}Albert"
 echo ""
 sleep 3
+
+
+
